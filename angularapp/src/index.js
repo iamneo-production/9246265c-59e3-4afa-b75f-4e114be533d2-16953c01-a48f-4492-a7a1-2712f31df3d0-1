18190e4c59213f5ca4b1138f55e9aeacc01d3241
sunilswizy <sunilsubash006@gmail.com>
2023-08-07 07:44:14 +0000

    msg

--- angularapp/src/index.js
+++ angularapp/src/index.js
@@ -18,3 +18,38 @@ console.log("FDsag")
 console.log("FDsag")
 console.log("FDsag")
 console.log("FDsag")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasf")
+}
